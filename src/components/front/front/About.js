78aafbc5f1a3642cc6618395583efb0c86a31f98
Akhtar47623 <waheed47623@gmail.com>
2024-05-15 15:06:01 +0500

    New Commit

--- src/components/front/front/About.js
+++ src/components/front/front/About.js
@@ -2,12 +2,7 @@ import React from 'react'
 
 const About = () => {
     return (
-<<<<<<< HEAD
-
-        <div>About commit</div>
-=======
-        <div>About</div>
->>>>>>> aa9b5a67eb6488a045ee644438a0f784c61cf612
+        <div>About My new Commit</div>
     )
 }
 
